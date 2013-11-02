39873ef083eb2aab2da5f6527f80c0b614c68d2a
brennan <brennan@sigma>
2013-11-01 21:00:06 -0400

    testing

---
+++ fops/src/fops/fops.java
@@ -0,0 +1,18 @@
+package fops;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brennan
+ */
+public class fops {
+    
+    public static void main(String[] args){
+        System.out.println("yep ok");
+    }
+    
+}
